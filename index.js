16a767794a7585dce358d125c328d77ab4814407
VINIA6 <viniciusdeassisazevedo@hotmail.com>
2023-04-12 22:43:40 -0300

    Update: Profram Comand Line Interface

--- index.js
+++ index.js
@@ -1,20 +1,64 @@
-const commander = require('commander')
+const {program} = require('commander')
 const DataBase = require('./database')
+const Heroi = require('./heroi')
+
 async function main(){
-    commander 
+
+    program 
         .version('v1')
         .option('-n, --nome [value]',"Nome do heroi:")
         .option('-p, --poder [value]',"Poder do heroi:")
-        .option('-c, --cadastrar',"Cadastrar heroi")
-        .parse(process.argv)
+        .option('-i, --id [value]',"Poder do heroi:")
 
+
+        .option('-c, --cadastrar',"Cadastrar heroi")
+        .option('-l, --listar',"Listar heroi")
+        .option('-r --remove',"Remove heroi")
+        .option('-a --atualizar [value]',"Atualizar heroi")
+        .parse(process.argv)
+        
+    const heroi = new Heroi(program)
     try {
-        if(commander.cadastrar){
-            // const result = await DataBase.cadastrar()
+        if(program.cadastrar){
+
+            delete heroi.id
+
+            const result = await DataBase.cadastrar(heroi)
+            if(!result){
+                console.error('Heroi não foi cadastrado')
+                return;
+            }
+            console.log("Heroi foi cadastrado")
+        }
+        if(program.listar){
+            const result =  await DataBase.listar()
+            console.log(result)
+            return;
+        }
+        if(program.remover){
+            const result = await DataBase.remover(heroi.id)
+            if(result === false){
+                console.error('Não foi possivel remover')
+                return;
+            }
+            console.log('Removido com sucesso')
+        }
+        if(program.atualizar){
+            const idParaAtualizar = parseInt(program.atualizar)
+            delete heroi.id
+            const dado = JSON.stringify(heroi)  
+            const heroiAtualizar = JSON.parse(dado)
+            const result =  await DataBase.atualizar(idParaAtualizar,heroiAtualizar)
+            if(!result){
+                console.error("Não foi possivel atualizar")
+                return;
+            }
+            console.log('Atualizado com sucesso')
         }
     } catch (error) {
         console.error('DEU RUIM',error)
     }
+
 }
 
 main()
